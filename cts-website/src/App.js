1fc0d28f6a3dcb636bc4a2310bce5979b312777a
CT Shen <32235978+ChunTingShen@users.noreply.github.com>
2024-10-08 01:47:37 -0400

    fix 404 html issue

--- cts-website/src/App.js
+++ cts-website/src/App.js
@@ -20,10 +20,12 @@ function App() {
         <main className="main-content">
           <Routes>
             <Route exact path="/" element={<AboutMe />} />
-            <Route path="/about" element={<AboutMe />} />
+            <Route exact path="/about" element={<AboutMe />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<AboutMe />} />
+            
           </Routes>
         </main>
 
